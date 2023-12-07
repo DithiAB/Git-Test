dd9592426549210a3c78d846dce1e42880ea357c
Dithi <145804988+Dithiiii@users.noreply.github.com>
2023-12-07 04:29:56 -0600

    Added new function completedTask in list component

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,10 +1,14 @@
-
+import {useState } from 'react'
 import './Todo.css'
 const List = ({elem}) => {
+    const[line, setLine]= useState(false)
+    const completedTask=()=>{
+        setLine(true)
+    }
  
   return (
     <div className='list'>
-        <p >{elem}</p>
+        <p onClick={completedTask}style={{textDecoration:line?'line-through':'none'}}>{elem}</p>
        
     </div>
   )
